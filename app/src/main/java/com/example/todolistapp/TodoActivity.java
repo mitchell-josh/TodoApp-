850f99fe16f863d60c24473fdf7b6fd54fc070c6
Ebbi <ebbi40@gmail.com>
2017-11-13 20:27:12 +0000

    controllers complete

---
+++ app/src/main/java/com/example/todolistapp/TodoActivity.java
@@ -0,0 +1,44 @@
+package com.example.todolistapp;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.UUID;
+
+public class TodoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TODO_ID = "todo_id";
+
+    public static Intent newIntent(Context packageContext, UUID todoId) {
+        Intent intent = new Intent(packageContext, TodoActivity.class);
+        intent.putExtra(EXTRA_TODO_ID, todoId);
+        return intent;
+    }
+/*
+    protected Fragment createFragment(){
+        UUID todoId = (UUID) getIntent().getSerializableExtra(EXTRA_TODO_ID);
+        return TodoFragment.newInstance(todoId);
+    }
+*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            TodoFragment todoFragment = new TodoFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, todoFragment)
+                    .commit();
+        }
+
+    }
+}
