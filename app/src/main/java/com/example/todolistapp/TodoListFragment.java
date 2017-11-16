2bf229ca5aa68e2ec026e2a19e3bd6bd52d4a97b
Ebbi <ebbi40@gmail.com>
2017-11-16 01:49:03 +0000

    Toolbar added

--- app/src/main/java/com/example/todolistapp/TodoListFragment.java
+++ app/src/main/java/com/example/todolistapp/TodoListFragment.java
@@ -1,10 +1,14 @@
 package com.example.todolistapp;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,6 +26,7 @@ public class TodoListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
 
     }
 
@@ -42,11 +47,35 @@ public class TodoListFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_todo_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_todo:
+
+                Todo todo = new Todo();
+                TodoModel.get(getActivity()).addTodo(todo);
+
+                Intent intent = TodoActivity.newIntent(getActivity(), todo.getId());
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
     public void onResume() {
         super.onResume();
         updateUI();
     }
-
 
     private void updateUI(){
 
@@ -130,4 +159,6 @@ public class TodoListFragment extends Fragment {
         }
 
     }
+
+
 }
